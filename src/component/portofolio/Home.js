32f0087953e583000858ad0dd441ea153f4af84c
vodapermadi <yapsitusaya111@gmail.com>
2022-05-26 07:21:24 +0700

    upload

--- src/component/portofolio/Home.js
+++ src/component/portofolio/Home.js
@@ -16,66 +16,81 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Home = () => {
-
-  AOS.init()
+  AOS.init();
 
   return (
     <>
       <NavbarComponent />
       <div className="bg1 py-5" style={{ fontFamily: "Nunito,sans-serif" }}>
-        <div className="container px-3" style={{ padding: "100px" }} id="home" data-aos={"fade-right"} data-aos-duration={"2000"}>
-          <div className="row shadow justify-content-center px-5 bgprofil" style={{ borderRadius : "30px" }} >
-            <div className="col-md-12 text-center py-5 text-white">
+        <div
+          className="container px-3 py-5 text-center"
+          style={{ padding: "100px" }}
+          id="home"
+          data-aos={"fade-right"}
+          data-aos-duration={"2000"}
+        >
               <div data-aos={"fade-left"}>
                 <h1>Name-Team</h1>
-                <h3>
-                here are the members of the team-name
-                </h3>
+                <h3>here are the members of the team-name</h3>
               </div>
+          <div
+            className="row shadow justify-content-center px-5 bgprofil"
+            style={{ borderRadius: "30px" }}
+          >
+            <div
+              className="col mx-2 mb-5 shadow text-center"
+              data-aos={"fade-up"}
+              data-aos-duration={"3000"}
+              style={{ borderRadius: "20px" }}
+            >
+              <h5 className="mb-2 text-white fw-bold">Front-End Dev</h5>
+              <img
+                src={voda}
+                alt="vodapermadi"
+                style={{ width: "300px" }}
+                className="img"
+              />
+              <a
+                className="btn buttonColor text-white mt-2"
+                style={{ width: "300px", outline: "2px solid #00A8CC" }}
+                href="#voda"
+              >
+                Voda Permadi
+              </a>
             </div>
-            <div className="col mx-2 mb-5 shadow py-4" data-aos={"fade-up"} data-aos-duration={"3000"} style={{ borderRadius : "20px" }}>
-              <p className="text-center">
-                <h5 className="mb-2 text-white fw-bold">Front-End Dev</h5>
-                <img src={voda} style={{ width: "300px" }} className="shadow" />
-                <a
-                  className="btn buttonColor text-white mt-2"
-                  style={{ width: "300px" , outline : "2px solid #00A8CC" }}
-                  href="#voda"
-                >
-                  Voda Permadi
-                </a>
-              </p>
-            </div>
-            <div className="col mx-2 mb-5 shadow py-4" data-aos={"fade-up"} data-aos-duration={"3000"} >
-              <p className="text-center">
-                <h5 className="mb-2 text-white fw-bold">UI/UX designer</h5>
-                <img
-                  src={sabrina}
-                  className="shadow"
-                  style={{ width: "300px" }}
-                />
-                <a
-                  className="btn buttonColor text-white mt-2"
-                  style={{ width: "300px" , outline : "2px solid #00A8CC" }}
-                  href="#sabrina"
-                >
-                  Sabrina Utami
-                </a>
-              </p>
+            <div
+              className="col mx-2 mb-5 shadow py-4 px-2 text-center"
+              data-aos={"fade-up"}
+              data-aos-duration={"3000"}
+            >
+              <h5 className="mb-2 text-white fw-bold">UI/UX designer</h5>
+              <img
+                src={sabrina}
+                alt="sabrina"
+                className="img"
+                style={{ width: "300px" }}
+              />
+              <a
+                className="btn buttonColor text-white mt-2"
+                style={{ width: "300px", outline: "2px solid #00A8CC" }}
+                href="#sabrina"
+              >
+                Sabrina Utami
+              </a>
             </div>
           </div>
         </div>
       </div>
-      <About/>
-      <Line/>
-      <Header/>
-      <AboutVoda/>
-      <SkillVoda/>
-      <ContactVoda/>
-      <Line/>
-      <HeaderSabrina/>
-      <AboutSabrina/>
-      <ContactSabrina/>
+      <About />
+      <Line />
+      <Header />
+      <AboutVoda />
+      <SkillVoda />
+      <ContactVoda />
+      <Line />
+      <HeaderSabrina />
+      <AboutSabrina />
+      <ContactSabrina />
     </>
   );
 };
